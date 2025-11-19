5af29da6b4b10381e1fa17c28b986650af73a595
syiffac <syifaasyikaa@gmail.com>
2025-11-20 03:13:21 +0700

    first commit

---
+++ lib/ui/registrasi_page.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+class RegistrasiPage extends StatefulWidget {
+  const RegistrasiPage({Key? key}) : super(key: key);
+
+  @override
+  _RegistrasiPageState createState() => _RegistrasiPageState();
+}
+
+class _RegistrasiPageState extends State<RegistrasiPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
+
+  final _namaTextboxController = TextEditingController();
+  final _emailTextboxController = TextEditingController();
+  final _passwordTextboxController = TextEditingController();
+  final _passwordKonfirmasiTextboxController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Registrasi")),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _namaTextField(),
+                _emailTextField(),
+                _passwordTextField(),
+                _passwordKonfirmasiTextField(),
+                _buttonRegistrasi(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Textbox Nama
+  Widget _namaTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: "Nama"),
+      keyboardType: TextInputType.text,
+      controller: _namaTextboxController,
+      validator: (value) {
+        if (value!.length < 3) {
+          return "Nama harus diisi minimal 3 karakter";
+        }
+        return null;
+      },
+    );
+  }
+
+  // Textbox Email
+  Widget _emailTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: "Email"),
+      keyboardType: TextInputType.emailAddress,
+      controller: _emailTextboxController,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Email harus diisi';
+        }
+
+        // Validasi format email
+        Pattern pattern =
+            r'^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$';
+        RegExp regex = RegExp(pattern.toString());
+        if (!regex.hasMatch(value)) {
+          return "Email tidak valid";
+        }
+        return null;
+      },
+    );
+  }
+
+  // Textbox Password
+  Widget _passwordTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: "Password"),
+      keyboardType: TextInputType.text,
+      obscureText: true,
+      controller: _passwordTextboxController,
+      validator: (value) {
+        if (value!.length < 6) {
+          return "Password harus diisi minimal 6 karakter";
+        }
+        return null;
+      },
+    );
+  }
+
+  // Textbox Konfirmasi Password
+  Widget _passwordKonfirmasiTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: "Konfirmasi Password"),
+      keyboardType: TextInputType.text,
+      obscureText: true,
+      validator: (value) {
+        if (value != _passwordTextboxController.text) {
+          return "Konfirmasi Password tidak sama";
+        }
+        return null;
+      },
+    );
+  }
+
+  // Tombol Registrasi
+  Widget _buttonRegistrasi() {
+    return ElevatedButton(
+      child: const Text("Registrasi"),
+      onPressed: () {
+        var validate = _formKey.currentState!.validate();
+
+        setState(() {
+          _isLoading = false;
+        });
+      },
+    );
+  }
+}
